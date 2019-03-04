190ec3b6c67944a2e80914f0c991c533a1eb2b65
Shibaji Chakraborty <shibaji7@nrlogin1.cluster>
2019-03-04 17:31:04 -0500

    Download SYM-H files from WDC Kyoto

--- src/dump_data.py
+++ src/dump_data.py
@@ -1,17 +1,33 @@
-import urllib2
+#!/usr/bin/env python
 
-############################
-## 
-############################
+"""dump_data.py: dump data downloads all the ascii type files from different FTP repositories."""
+
+__author__ = "Chakraborty, S."
+__copyright__ = "Copyright 2007, Space @ VT"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+
+
+import os
+import urllib2
+import numpy as np
+import datetime as dt
+from calendar import monthrange
+
+##############################################################################################
+## Download 1m resolution solar wind omni data from NASA GSFC ftp server
+##############################################################################################
 def download_omni_dataset():
     base_uri = "ftp://spdf.gsfc.nasa.gov/pub/data/omni/high_res_omni/monthly_1min/omni_min%d%02d.asc"
     base_storage = "/home/shibaji7/DATA/omni/raw/%d%02d.asc"
     for year in range(1995,2019):
         for month in range(1,13):
             url = base_uri%(year,month)
-            print url
             response = urllib2.urlopen(url)
-            print url, response.getcode()
             f_path = base_storage%(year,month)
             with open(f_path,"w") as f:
                 f.write(response.read())
@@ -21,7 +37,28 @@ def download_omni_dataset():
     return
 
 
-
+##############################################################################################
+## Download 1m resolution ASY / SYM data from WDC Kyoto ftp server
+##############################################################################################
+def download_symH_dataset():
+    base_uri = "http://wdc.kugi.kyoto-u.ac.jp/cgi-bin/aeasy-cgi?Tens=%s&Year=%s&Month=%s&Day_Tens=0&Days=1&Hour=00&min=00&Dur_Day_Tens=%s&Dur_Day=%s&Dur_Hour=00&Dur_Min=00&Image+Type=GIF&COLOR=COLOR&AE+Sensitivity=0&ASY/SYM++Sensitivity=0&Output=ASY&Out+format=IAGA2002&Email=shibaji7@vt.edu"
+    base_storage = "/home/shibaji7/DATA/geomag/symh/raw/%d%02d.asc"
+    for year in range(1995, 2019):
+        for month in range(1,13):
+            ytens = str(int(year/10))
+            yrs = str(np.mod(year,10))
+            mnt = "%02d"%month
+            _,dur = monthrange(year, month)
+            durtens = "%02d"%(int(dur/10))
+            dur = str(np.mod(dur,10))
+            url = base_uri%(ytens,yrs,mnt,durtens,dur)
+            f_path = base_storage%(year,month)
+            cmd = "wget -O '%s' '%s'"%(f_path, url)
+            os.system(cmd)
+            pass
+        pass
+    return
+ 
 if __name__ == "__main__":
-    download_omni_dataset()
+    download_symH_dataset()
     pass
---
+++ src/process_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""process_data.py: process data do a multi-stage processing of all the ascii type files and save as .hdf5 file store."""
+
+__author__ = "Chakraborty, S."
+__copyright__ = "Copyright 2007, Space @ VT"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
