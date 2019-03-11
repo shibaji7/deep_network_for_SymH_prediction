f2122e9707be6ebd356857b3c0b0aa97ae5b4b51
Shibaji Chakraborty <shibaji7@nrlogin1.cluster>
2019-03-11 13:08:22 -0400

    Batch jobs and VT ARC installation.

--- configure.sh
+++ configure.sh
@@ -1,3 +1,6 @@
-anaconda
+module purge
+module load Anaconda
 conda create -n Deep python=2.7
 source activate Deep
+conda list --export > requirements.txt
+while read requirement; do conda install --yes $requirement; done < requirements.txt
--- jobsub.sh
+++ jobsub.sh
@@ -6,5 +6,10 @@
 #PBS -W group_list=newriver
 #PBS -M shibaji7@vt.edu
 #PBS -m bea
+cd $PBS_O_WORKDIR
+module purge
+module load Anaconda
+source activate Deep
+export PYTHONUSERBASE=/home/shibaji7/deep_network_for_SymH_prediction/
 python main.py
 exit;
